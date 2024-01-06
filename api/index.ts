2ae0471043829cc7c461c591eea018bb650709b6
Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>
2024-01-06 14:32:36 +0800

    chore(global): migrate source files to src directory
    Also make API routes use switch-case syntax instead of if-then statements,
    among other things.
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>

--- api/index.ts
+++ api/index.ts
@@ -3,29 +3,34 @@ import { IncomingRequestCf } from './deps.ts';
 export default {
     fetch(request: IncomingRequestCf, env: any) {
         const urlParser = new URL(request.url)
-        if (urlParser.pathname == "/api") {
-            return new Response("Hello, world!", { status: 200 })
-        } else if (urlParser.pathname == "/api/debug") {
-            const data = JSON.stringify({
-                ok: true,
-                cdnCallback: {
-                    botManagement: request.cf.botManagement,
-                    colocation: request.cf.colo
-                },
-                requestData: {
-                    ip: request.headers.get("CF-Connecting-IP"),
-                    tz: `${'timezone' in request.cf ? request.cf.timezone : "UTC"}`,
-                    asn: request.cf.asn,
-                    asOrg: request.cf.asOrganization,
-                    city: `${'city' in request.cf ? request.cf.city : null }`,
-                    ua: request.headers.get("User-Agent")
-                }
-            })
-            return new Response(data, {
-                status: 200,
-                headers: { 'Content-Type': 'application/json'}
-            })
+
+        switch (urlParser.pathname) {
+            case '/api': {
+                return new Response("Hello, world!", { status: 200 });
+            }
+            case '/api/debug': {
+                const data = JSON.stringify({
+                    ok: true,
+                    cdnCallback: {
+                        botManagement: request.cf.botManagement,
+                        colocation: request.cf.colo
+                    },
+                    requestData: {
+                        ip: request.headers.get("CF-Connecting-IP"),
+                        tz: `${'timezone' in request.cf ? request.cf.timezone : "UTC"}`,
+                        asn: request.cf.asn,
+                        asOrg: request.cf.asOrganization,
+                        city: `${'city' in request.cf ? request.cf.city : null }`,
+                        ua: request.headers.get("User-Agent")
+                    }
+                })
+                return new Response(data, {
+                    status: 200,
+                    headers: { 'Content-Type': 'application/json'}
+                })
+            }
         }
+
         const default404Reply = JSON.stringify({
             ok: false,
             error: "API endpoint not found"
