b714a2c5c63071ea33c93f923ff54b3b4fd2e5d7
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-01-08 23:42:26 +0800

    chore(global): setup nix-based devenv, plus update deps
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

---
+++ devenv.nix
@@ -0,0 +1,51 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    gitFull
+    pipenv
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+  languages.python = {
+    enable = true;
+    package = pkgs.python313;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  enterShell = ''
+    pipenv install
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
